0f036f42b799e4e863a609fd091630876907d44b
Mesbah Uddin <msbh214@gmail.com>
2024-03-04 21:19:17 +0600

    primary project structure and resources are set

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,55 @@
-function defaultTask(cb) {
-  // place code for your default task here
+const { src, dest, watch, series } = require("gulp");
+const browsersync = require("browser-sync").create();
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("autoprefixer");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+
+const files = {
+  scssPath: "app/scss/**/*.scss",
+};
+
+// ScSS Task
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest("dist/css/"))
+    .pipe(browsersync.stream());
+}
+
+function css(cb) {
+  // body omitted
   cb();
 }
 
-exports.default = defaultTask
+function javascript(cb) {
+  // body omitted
+  cb();
+}
+
+// Browser-sync Task
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: "dist",
+    },
+  });
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch("dist/**/*.html", browsersyncReload);
+  watch(files.scssPath, scssTask);
+}
+
+// Default gulp Task
+exports.default = series(scssTask, browsersyncServe, watchTask);
+
+exports.build = series(scssTask);
