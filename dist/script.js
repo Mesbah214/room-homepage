135f040d921439b149aa613938dab6ea6be928b3
Mesbah Uddin <msbh214@gmail.com>
2024-03-06 10:37:04 +0600

    slider is set & hero section is updated

---
+++ dist/script.js
@@ -0,0 +1,29 @@
+// Slider
+(function slider() {
+  const imgSlider = document.querySelector(".header__img-container");
+  const textSlider = document.querySelector(".header__hero-copy-container");
+
+  const prevBtn = document.getElementById("prev");
+  const nextBtn = document.getElementById("next");
+
+  prevBtn.addEventListener("click", prevBtnClickHandler);
+  nextBtn.addEventListener("click", nextBtnClickHandler);
+
+  let counter = 0;
+
+  function prevBtnClickHandler() {
+    if (counter > 0) counter--;
+    imgSlider.style.transform = "translateX("+ -counter * 33.33 +"%)";
+    textSlider.style.transform = "translateX("+ -counter * 33.33 +"%)";
+    // textSlider.style.transform = "translateX(20%)";
+    console.log(imgSlider)
+  }
+
+  function nextBtnClickHandler() {
+    if (counter < 2) counter++;
+    imgSlider.style.transform = "translateX("+ -counter * 33.33 +"%)";
+    textSlider.style.transform = "translateX("+ -counter * 33.33 +"%)";
+    // textSlider.style.transform = "translateX(20%)";
+    console.log(textSlider)
+  }
+})();
